a20cb7f82994043ae7f158551c0b92abe64aeb49
arin-s <sarkisianarin@gmail.com>
2026-01-23 05:04:19 +1100

    change: disable sleeping and calibrate the timer (this shouldn't change anything but just in case)

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if make -j "$(nproc)" T=open_source DEBUG=1 NOAPP=1 MCU_HIGH_PERFORMANCE_MODE=1 FLASH_LOW_SPEED=1 PSRAM_LOW_SPEED=0 DOOMBUDS=1 V=1 LARGE_RAM=1 ANC_APP=0 >log.txt 2>&1; then
+if make -j "$(nproc)" T=open_source DEBUG=1 NOAPP=1 MCU_HIGH_PERFORMANCE_MODE=1 NO_SLEEP=1 CALIB_SLOW_TIMER=1 FLASH_LOW_SPEED=1 PSRAM_LOW_SPEED=0 DOOMBUDS=1 V=1 LARGE_RAM=1 ANC_APP=0 >log.txt 2>&1; then
 	echo "build success"
 else
 	echo "build failed and call log.txt"
